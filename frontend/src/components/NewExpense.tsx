1db53e3ff389c49e9f98bdb1cd190828191c1f4e
redarrow025 <re035009@ucf.edu>
2024-12-04 14:42:38 -0500

    Update NewExpense.tsx

--- frontend/src/components/NewExpense.tsx
+++ frontend/src/components/NewExpense.tsx
@@ -40,12 +40,14 @@ export default function NewExpense() {
     const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setDropDownValue(event.target.value); // Update state with the selected value
       };
-    
-      const handleValueChange = (e) => {
-        const newValue = parseFloat(event.target.value);
-        setCurrencyValue(newValue.target.value); 
-        // Do something with the value, like storing it in state or sending it to an API
-      };
+
+      const handleValueChange = (value: string | undefined) => {
+        if (value) {
+            setCurrencyValue(parseFloat(value)); // Parse the string value to a float
+        } else {
+            setCurrencyValue(0.0); // Default to 0 if no value is provided
+        }
+    };
       const handleNotesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setNotes(event.target.value);
       }
